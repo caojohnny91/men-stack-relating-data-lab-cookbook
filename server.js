f308f00b6de8d9fb2c446dc622e857c7649908ef
jcao91 <jcao91@yahoo.com>
2024-06-08 14:05:23 -0500

    middleware created and required

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
 
 const authController = require("./controllers/auth.js");
 const foodsController = require("./routes/foods.js"); // using routes instead of controllers for better organization
@@ -43,7 +45,9 @@ app.get("/vip-lounge", (req, res) => {
   }
 });
 
+app.use(passUserToView)
 app.use("/auth", authController);
+app.use(isSignedIn)
 app.use('/users/:userId/foods', foodsController); 
 
 app.listen(port, () => {
